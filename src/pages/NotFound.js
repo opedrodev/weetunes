9eab6e2e26af7732933455a19b1cc58fe9bf96c8
opedrodev <pvasilva.dev@hotmail.com>
2022-10-24 00:01:11 -0300

    Add not found page style

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/NotFound.scss';
 
 class NotFound extends Component {
   render() {
     return (
-      <div data-testid="page-not-found">
-        <p>404 Page Not Found</p>
+      <div className="NotFound">
+        <div>
+          <p>404 Page Not Found</p>
+          <Link to="/">Go Home</Link>
+        </div>
       </div>
     );
   }
